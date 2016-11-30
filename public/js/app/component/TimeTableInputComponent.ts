41e4e7503dcfa5ce9fbf9a65676e78d7abbe5242
almiraj <rain@almiraj.net>
2016-11-30 23:22:32 +0900

    Add TimeRowSummaryPipe

--- public/js/app/component/TimeTableInputComponent.ts
+++ public/js/app/component/TimeTableInputComponent.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonService } from '../service/CommonService';
 import { TimeTableComponent } from '../component/TimeTableComponent';
 import { TimeTableService } from '../service/TimeTableService';
 
 @Component({
   selector: 'TimeTableInputComponent',
-  providers: [TimeTableService, CommonService],
+  providers: [TimeTableService],
   template: `
     <TimeTableComponent></TimeTableComponent>
     <button class="btn btn-default pull-right" (click)="save()">保存</button>
--- public/js/app/service/CommonService.ts
+++
@@ -1,9 +0,0 @@
-export class CommonService {
-  expand4digit(num:Number):String {
-    const str = String(num);
-    if (str.length >= 4) {
-        return str;
-    }
-    return ('0000' + str).slice(-4);
-  }
-}
