00bd8c4af840a2b9eceabe564b933fb2af182bd7
almiraj <rain@almiraj.net>
2017-03-21 13:58:10 +0900

    Refactor setup.js from UPDATE to DELETE-INSERT

--- app/ws/setup.js
+++ app/ws/setup.js
@@ -2,72 +2,54 @@ const AccountModel = require('../model/AccountModel');
 const TimeSheetUsersModel = require('../model/TimeSheetUsersModel');
 
 module.exports = function(req) {
-  return Promise.all([
-    new Promise((resolve, reject) => {
-      AccountModel.update(
-        {
-          userId: 'foo'
-        },
-        {
-          userHash: 'b65e5ef9b569432539dba222ddc32ee173ef1812d0638ce7fc7f75561a9ae179' // bar
-        },
-        { upsert: true },
-        (err) => !err ? resolve() : reject(err)
-      )
-    }),
-    new Promise((resolve, reject) => {
-      AccountModel.update(
-        {
-          userId: 'foo2'
-        },
-        {
-          userHash: 'b65e5ef9b569432539dba222ddc32ee173ef1812d0638ce7fc7f75561a9ae179' // bar
-        },
-        { upsert: true },
-        (err) => !err ? resolve() : reject(err)
-      )
-    }),
-    new Promise((resolve, reject) => {
-      TimeSheetUsersModel.update(
-        {
-          userId: 'foo'
-        },
-        {
-          timeSheets: [
-            {
-              month: '201611',
-              dateRows: [ { date: '1', begin: '0900', end: '1800', interval: '0100' }, { date: '2', begin: '0900', end: '1800', interval: '0100' } ]
-            },
-            {
-              month: '201612',
-              dateRows: [ { date: '1', begin: '0930', end: '2100', interval: '0100' }, { date: '2', begin: '1230', end: '2400', interval: '0100' } ]
-            }
-          ]
-        },
-        { upsert: true },
-        (err) => !err ? resolve() : reject(err)
-      )
-    }),
-    new Promise((resolve, reject) => {
-      TimeSheetUsersModel.update(
-        {
-          userId: 'foo2'
-        },
-        {
-          timeSheets: [
-            {
-              month: '201611',
-              dateRows: [ { date: '1', begin: '0900', end: '1800', interval: '0100' }, { date: '2', begin: '0900', end: '1800', interval: '0100' } ]
-            },
-            {
-              month: '201612',
-              dateRows: [ { date: '1', begin: '0930', end: '2100', interval: '0100' }, { date: '2', begin: '1230', end: '2400', interval: '0100' } ]
-            }
-          ]
-        },
-        { upsert: true },
-        (err) => !err ? resolve() : reject(err)
-      )
+  return Promise.resolve()
+    .then(() => {
+      new Promise((resolve, reject) => {
+        return AccountModel.remove((err) => !err ? resolve() : reject(err));
+      });
     })
-  ]);
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        return TimeSheetUsersModel.remove((err) => !err ? resolve() : reject(err));
+      });
+    })
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        AccountModel.collection.insert([
+          new AccountModel(
+            {
+              userId: 'foo',
+              userHash: 'b65e5ef9b569432539dba222ddc32ee173ef1812d0638ce7fc7f75561a9ae179' // bar
+            }
+          ),
+          new AccountModel(
+            {
+              userId: 'admin',
+              userHash: 'fbbd5f9ac5423983deb5a7b00a27b0ddc193828e0c6a4dfe957ad1300d963a51' // admin
+            }
+          )
+        ], (err) => !err ? resolve() : reject(err));
+      });
+    })
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        TimeSheetUsersModel.collection.insert([
+          new TimeSheetUsersModel(
+            {
+              userId: 'foo',
+              timeSheets: [
+                {
+                  month: '201611',
+                  dateRows: [ { date: '1', begin: '0900', end: '1800', interval: '0100' }, { date: '2', begin: '0900', end: '1800', interval: '0100' } ]
+                },
+                {
+                  month: '201612',
+                  dateRows: [ { date: '1', begin: '0930', end: '2100', interval: '0100' }, { date: '2', begin: '1230', end: '2400', interval: '0100' } ]
+                }
+              ]
+            }
+          )
+        ], (err) => !err ? resolve() : reject(err));
+      });
+    });
 };
