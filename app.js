94473e201822ecbc5e62f728c70d8849cbeb67f3
almiraj <rain@almiraj.net>
2019-01-21 17:24:59 +0900

    Allow CORS

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var routes = require('./app/router');
 
 var app = express();
 
+// CORSを許可する
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'jade');
