b4e78d37d7c00b83ac3df9d6c9141a4d1663849e
almiraj <rain@almiraj.net>
2019-02-05 19:29:06 +0900

    Add icon and splash with cordova-res-generator

---
+++ hooks/copy-ios-icon-splash.bat
@@ -0,0 +1,2 @@
+copy resources\ios\icon\* platforms\ios\Alexandrite\Images.xcassets\AppIcon.appiconset\
+copy resources\ios\splash\* platforms\ios\Alexandrite\Images.xcassets\LaunchImage.launchimage\
---
+++ hooks/copy-ios-icon-splash.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -f resources/ios/icon/* platforms/ios/Alexandrite/Images.xcassets/AppIcon.appiconset/
+cp -f resources/ios/splash/* platforms/ios/Alexandrite/Images.xcassets/LaunchImage.launchimage/
